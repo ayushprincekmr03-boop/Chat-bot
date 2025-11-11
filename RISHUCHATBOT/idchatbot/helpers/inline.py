ccd242c09b5050139d7fe9f73a06a19cf4fd24c4
𝐊𝐀𝐍𝐇𝐀 <ayushprincekmr03@gmail.com>
2025-11-12 01:28:21 +0530

    Update inline.py

--- RISHUCHATBOT/idchatbot/helpers/inline.py
+++ RISHUCHATBOT/idchatbot/helpers/inline.py
@@ -16,7 +16,7 @@ DEV_OP = [
     [
         InlineKeyboardButton(
             text="❍ 𝐀ᴅᴅ 𝐌ᴇ 𝐘ᴏᴜʀ 𝐆ʀᴏᴜᴘ ❍",
-            url=f"https://t.me/{RISHUCHATBOT.username}?startgroup=true",
+            url=f"https://t.me/{oyekanhaa.username}?startgroup=true",
         ),
     ],
     [
